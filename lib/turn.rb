1f34d20291de14a3288c33ae3b00c6b09c94d023
Mayra  Salazar <mayrasalazar@unitec.edu>
2018-01-25 20:10:01 +0000

    automatically backed up by learn

--- lib/turn.rb
+++ lib/turn.rb
@@ -9,3 +9,17 @@ end
 def input_to_index(input)
   input.to_i
 end
+
+def valid_move?(board, index)
+  if index.between?(0,8) && position_taken?(board,index) == false
+    return true
+  end
+  return false
+end
+
+def position_taken?(board, index)
+  if board[index] == " " || board[index] == nil || board[index] == ""
+    return false
+  end
+  return true
+end
